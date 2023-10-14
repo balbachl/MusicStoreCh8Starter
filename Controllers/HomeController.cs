46c12b47212cd7692a4cf0b9d2667d2a6399f30e
Lisa Balbach <lisajbalbach@gmail.com>
2023-10-14 19:03:19 -0400

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using MusicStoeCh6.Models;
+using MusicStoreCh6.Models;
 using MusicStore.Models;
 
 namespace MusicStoreCh6.Controllers
